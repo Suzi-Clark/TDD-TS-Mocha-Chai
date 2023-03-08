1feb8bf8d85d3b00f61042623e75c8094469e9ea
Suzi-Clark <susannaclark95@gmail.com>
2023-03-08 16:29:57 +0000

    added more tests

--- src/vowelCount.spec.ts
+++ src/vowelCount.spec.ts
@@ -6,3 +6,21 @@ describe("When vowelCount is called", () => {
         expect(vowelCount('boat soap')).to.equal(4);
     });
 });
+
+describe("When vowelCount is called", () => {
+    it("with 'zzz pp vvv' it returns 0", () => {
+        expect(vowelCount('zzz pp vvv')).to.equal(0);
+    });
+});
+
+describe("When vowelCount is called", () => {
+    it("with 'lorem ipsum dolor sit amet consectetur adipiscing elit' it returns 18", () => {
+        expect(vowelCount('lorem ipsum dolor sit amet consectetur adipiscing elit')).to.equal(18);
+    });
+});
+
+describe("When vowelCount is called", () => {
+    it("with null it returns 0", () => {
+        expect(vowelCount(null)).to.equal(0);
+    });
+});
